56212e72825edd8315f2b4cd8e518e926b12fdc4
krating20 <56456483+krating20@users.noreply.github.com>
2024-02-16 16:41:54 +0700

    states added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import Header from "./components/Header"
 
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackStats from "./components/FeedbackStats";
 function App() {
+    const[feedback,setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback =(id) =>{
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item)=>item.id!==id))
+        }
+        
+    }
   return (
     <>
     <Header/>
     <div className="container">
-      <h1>Hello from the app component</h1>
+    <FeedbackStats feedback={feedback}/>
+    <FeedbackList feedback={feedback}
+    handleDelete={deleteFeedback}/>
     </div>
     </>
   );
