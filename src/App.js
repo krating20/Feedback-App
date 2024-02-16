843f7e13d6c2286f8058b8af9506b555896474aa
krating20 <56456483+krating20@users.noreply.github.com>
2024-02-16 19:39:59 +0700

    added form validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import Header from "./components/Header"
-
+import { v4 as uuidv4} from 'uuid'
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackItem from "./components/FeedbackItem";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
     const[feedback,setFeedback] = useState(FeedbackData)
-
+    const addFeedback = (newFeedback,) =>{
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+        console.log(newFeedback)
+    }
     const deleteFeedback =(id) =>{
         if(window.confirm('Are you sure you want to delete?')){
             setFeedback(feedback.filter((item)=>item.id!==id))
@@ -18,6 +23,7 @@ function App() {
     <>
     <Header/>
     <div className="container">
+    <FeedbackForm handleAdd={addFeedback}/>
     <FeedbackStats feedback={feedback}/>
     <FeedbackList feedback={feedback}
     handleDelete={deleteFeedback}/>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
+import {motion,AnimatePresence} from 'framer-motion'
 function FeedbackList({feedback,handleDelete}) {
     if(!feedback || feedback.length ===0){
         return <p>no feedback yet</p>
     }
 
   return  <div className='feedback-list'>
+    <AnimatePresence>
    {feedback.map((item) =>(
+    <motion.div 
+    key={item.id}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    layout
+    >
         <FeedbackItem key={item.id} item = {item}
         handleDelete= {handleDelete}/>
-   ))}
+    </motion.div>
+   ))} </AnimatePresence>
     </div>
+   
   
 }
 
